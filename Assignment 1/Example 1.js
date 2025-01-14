739858c63983f309a80039b94ec37d9c91fbfc2a
Caitlyn G. <cqitlyng@gmail.com>
2025-01-14 16:33:06 -0600

    Assignment 1

---
+++ Assignment 1/Example 1.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(400, 200);
+  }
+  
+  function draw() {
+    background("lime");
+    strokeWeight(2);
+    circle(100, 100, 160);
+    square(215, 20, 160);
+  }
+  
